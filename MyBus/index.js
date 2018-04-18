a5cf66f71bbcc618de915d8a69d50dcf3c4bcc1f
vicky4050 <arunrajappan@gmail.com>
2018-04-18 12:05:10 -0400

    Update index.js

--- MyBus/index.js
+++ MyBus/index.js
@@ -20,88 +20,14 @@ exports.handler = (event, context) => {
 
         switch(event.request.intent.name) {
           case "GetBusSchedule":
-            init();
-          
-          function init(){
-            var busId = 163
-            var stopId = 13456
-          
-            var endpoint = "http://mybusnow.njtransit.com/bustime/eta/getStopPredictionsETA.jsp?route="+busId+"&stop="+stopId+"&key=0.35774394008078714";
-            var body = ""
-            https.get(endpoint, (response) => {
-              response.on('data', (chunk) => { body += chunk })
-              response.on('end', () => {parseString(body, handler) })
-            });
-          }
-          
-          function handler(err, result) {
-            var response_message = ParseJson(result);
-            console.log(response_message);
-            context.succeed(
-            generateResponse(buildSpeechletResponse(response_message, true), {} )
-          )
-          }
-          
-          function ParseJson(result) {
-            var announceMsg, announceMsg1, announceMsg2;
-            var intrestedBusIteration = 2;
-            var options = {
-              expressOnlyBus : true,
-              ignoreBus : 'LOCAL|VIA UNION'
-            }
-          
-            if(result.stop.pre){
-              let timeList = createBusObj(result.stop.pre, options);
-              
-              if(timeList.length == 0){
-                 return "Bus data is unavailable.";     
-              }
-              
-              announceMsg = timeList[0].name + " is ";    
-              for (var i = 0, len = timeList.length; i < len && i < intrestedBusIteration; i++) {
-                
-                if(i == 1)  announceMsg += " and the next bus is ";
-          
-                d = timeList[i];
-                announceMsg +=  d.time + " "+ d.minute;		  
-              }
-            }else if(result.stop.noPredictionMessage){
-              announceMsg = "Bus data is unavailable.";
-            }  
-            return announceMsg;
-          }
-          function createBusObj(timeList, options){
-            var objArr = [];
-            for (var i = 0, len = timeList.length; i < len; i++) {
-              let bObj = new BusObj(timeList[i]);
-          
-          
-              var rgxp = new RegExp(options.ignoreBus, "g");
-              if(options.expressOnlyBus){
-                if(!bObj.name.match(rgxp))
-                  objArr[i] = bObj; 
-              }
-              else{
-                objArr[i] = bObj; 
-              }
-          }
-            //console.log(  objArr);
-            return  objArr;
-          }
-          
-          function BusObj(b){
-            this.name = b.fd[0];
-            this.time = b.pt[0];
-            this.min = b.pu[0];
-            this.qualifier = ' away'; 
-          
-            if(this.time == '&nbsp;'){
-              this.time = '';
-              this.qualifier = '';  
-              }
-              this.minute = this.min + this.qualifier;  
-            }
-            
+            var busId = 163;
+            var stopId = 13456;
+            init(busId, stopId);
+            break;
+          case "GetBusScheduleExpress":
+            var busId = 144;
+            var stopId = 12460;
+            init(busId, stopId);
             break;
           default:
             throw "Invalid intent"
@@ -118,6 +44,83 @@ exports.handler = (event, context) => {
         context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
 
     }
+    
+    
+            function init(busId, stopId){
+            
+              var endpoint = "http://mybusnow.njtransit.com/bustime/eta/getStopPredictionsETA.jsp?route="+busId+"&stop="+stopId+"";
+              var body = ""
+              https.get(endpoint, (response) => {
+                response.on('data', (chunk) => { body += chunk })
+                response.on('end', () => {parseString(body, handler) })
+              });
+            }
+            
+            function handler(err, result) {
+              var response_message = ParseJson(result);
+              console.log(response_message);
+              context.succeed(generateResponse(buildSpeechletResponse(response_message, true), {} ));
+            }
+            
+            function ParseJson(result) {
+              var announceMsg, announceMsg1, announceMsg2;
+              var intrestedBusIteration = 2;
+              var options = {
+                expressOnlyBus : true,
+                ignoreBus : 'LOCAL|VIA UNION'
+              }
+            
+              if(result.stop.pre){
+                let timeList = createBusObj(result.stop.pre, options);
+                
+                if(timeList.length == 0){
+                   return "Bus data is unavailable.";     
+                }
+                
+                announceMsg = timeList[0].name + " is ";    
+                for (var i = 0, len = timeList.length; i < len && i < intrestedBusIteration; i++) {
+                  
+                  if(i == 1)  announceMsg += " and the next bus is ";
+            
+                  d = timeList[i];
+                  announceMsg +=  d.time + " "+ d.minute;		  
+                }
+              }else if(result.stop.noPredictionMessage){
+                announceMsg = "Bus data is unavailable.";
+              }  
+              return announceMsg;
+            }
+            function createBusObj(timeList, options){
+              var objArr = [];
+              for (var i = 0, len = timeList.length; i < len; i++) {
+                let bObj = new BusObj(timeList[i]);
+            
+            
+                var rgxp = new RegExp(options.ignoreBus, "g");
+                if(options.expressOnlyBus){
+                  if(!bObj.name.match(rgxp))
+                    objArr[i] = bObj; 
+                }
+                else{
+                  objArr[i] = bObj; 
+                }
+            }
+              //console.log(  objArr);
+              return  objArr;
+            }
+            
+            function BusObj(b){
+              this.name = b.fd[0];
+              this.time = b.pt[0];
+              this.min = b.pu[0];
+              this.qualifier = ' away'; 
+            
+              if(this.time == '&nbsp;'){
+                this.time = '';
+                this.qualifier = '';  
+              }
+              this.minute = this.min + this.qualifier;  
+            }
 
   } catch(error) { context.fail(`Exception: ${error}`) }
 
